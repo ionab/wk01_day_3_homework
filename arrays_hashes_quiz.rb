4d1464ff6a616866b29d17815cb4d8db399a3806
Kelsie Braidwood <kelsieiona@gmail.com>
2018-03-28 16:34:22 +0100

    Question A10 first attempt

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -7,27 +7,36 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 ### Complete these tasks:
+#
+# # 1. Add `"Edinburgh Waverley"` to the end of the array
+# stops.push("Edinburgh Waverley")
+# # 2. Add `"Glasgow Queen St"` to the start of the array
+# stops.unshift("Glasgow Queen Street")
+# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops.insert(4, "Polmont")
+# # 4. Work out the index position of `"Linlithgow"`
+# p stops.index("Linlithgow")
+# # 5. Remove `"Livingston"` from the array using its name
+# stops.delete("Livingston")
+# # 6. Delete `"Cumbernauld"` from the array by index
+# stops.delete_at(2)
+# p stops
+# # 7. How many stops there are in the array?
+# p stops.length
+# # 8. How many ways can we return `"Falkirk High"` from the array?
+# p stops[2] #one way - by calling it by it's index
+# # 9. Reverse the positions of the stops in the array
+# p stops.reverse
+# # 10. Print out all the stops using a for loop
+for station in stops
+  stations_with_a = []
+    if station.include?("a")
+      stations_with_a.push(station)
+    end
+  p stations_with_a
+end
 
-# 1. Add `"Edinburgh Waverley"` to the end of the array
-stops.push("Edinburgh Waverley")
-# 2. Add `"Glasgow Queen St"` to the start of the array
-stops.unshift("Glasgow Queen Street")
-# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-stops.insert(4, "Polmont")
-# 4. Work out the index position of `"Linlithgow"`
-p stops.index("Linlithgow")
-# 5. Remove `"Livingston"` from the array using its name
-stops.delete("Livingston")
-# 6. Delete `"Cumbernauld"` from the array by index
-stops.delete_at(2)
-p stops
-# 7. How many stops there are in the array?
-p stops.length
-# 8. How many ways can we return `"Falkirk High"` from the array?
-p stops[2] #one way - by calling it by it's index
-# 9. Reverse the positions of the stops in the array
-p stops.reverse
-# 10. Print out all the stops using a for loop
+
 
 ## Exercise B
 
